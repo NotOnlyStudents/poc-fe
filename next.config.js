3b56f2a198501e513e1ef4a2979eb10c153e693f
Lorenzo Brescanzin <30345822+br3sc4@users.noreply.github.com>
2021-02-11 11:09:20 +0100

    Configurato AWS Cognito

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  switch (phase) {
+    case PHASE_DEVELOPMENT_SERVER:
+      return {
+        // environment varibales for local development
+        env: {
+          IDP_DOMAIN: "emporiolambda.auth.eu-west-1.amazoncognito.com",
+          USER_POOL_ID: "eu-west-1_g3tY2ay6y",
+          USER_POOL_CLIENT_ID: "2p5i1sksdg7res1s6hrdo5ckk8",
+          REDIRECT_SIGN_IN: "http://localhost:3000/token",
+          REDIRECT_SIGN_OUT: "http://localhost:3000/",
+          AUTH_COOKIE_DOMAIN: "localhost",
+        },
+      };
+    default:
+      return {
+        // environment varibales for production
+        env: {
+          IDP_DOMAIN: "nextjs-example-prod.auth.eu-central-1.amazoncognito.com",
+          USER_POOL_ID: "eu-central-1_p25eoCGW4",
+          USER_POOL_CLIENT_ID: "66hpug32jqnemqg59bha64pvds",
+          REDIRECT_SIGN_IN: "https://aws-cognito-next-example-app.now.sh/token",
+          REDIRECT_SIGN_OUT: "https://aws-cognito-next-example-app.now.sh/",
+          AUTH_COOKIE_DOMAIN: "aws-cognito-next-example-app.now.sh",
+        },
+      };
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,48 @@ import Layout, { siteTitle } from 'components/layout'
 import utilStyles from 'styles/utils.module.css'
 // import { getSortedPostsData } from '../lib/posts'
 import { GetStaticProps } from 'next'
-import Amplify from 'aws-amplify'
+import Amplify from "@aws-amplify/core";
+import Auth from "@aws-amplify/auth";
 import { AmplifySignIn, withAuthenticator } from '@aws-amplify/ui-react'
+
+Amplify.configure({
+  Auth: {
+    region: process.env.USER_POOL_REGION,
+    userPoolId: process.env.USER_POOL_ID,
+    userPoolWebClientId: process.env.USER_POOL_CLIENT_ID,
+
+    // Configuration for cookie storage
+    // see https://aws-amplify.github.io/docs/js/authentication
+    cookieStorage: {
+      // REQUIRED - Cookie domain
+      // This should be the subdomain in production as
+      // the cookie should only be present for the current site
+      domain: process.env.AUTH_COOKIE_DOMAIN,
+      // OPTIONAL - Cookie path
+      path: "/",
+      // OPTIONAL - Cookie expiration in days
+      expires: 7,
+      // OPTIONAL - Cookie secure flag
+      // Either true or false, indicating whether the cookie
+      // transmission requires a secure protocol (https).
+      // The cookie should be set to secure in production.
+      secure: false,
+    },
+  }
+});
+
+Auth.configure({
+  oauth: {
+    domain: process.env.IDP_DOMAIN,
+    scope: ["email", "openid"],
+    // Where users get sent after logging in.
+    // This has to be set to be the full URL of the /token page.
+    redirectSignIn: process.env.REDIRECT_SIGN_IN,
+    // Where users are sent after they sign out.
+    redirectSignOut: process.env.REDIRECT_SIGN_OUT,
+    responseType: "token"
+  },
+});
 
 export default function Home({
   allPostsData
