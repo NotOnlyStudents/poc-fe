98a86c064d7e8c880e296d7e1899768bf87c3beb
Matteo <matteo16.martini@gmail.com>
2021-02-11 11:25:42 +0100

    Funziona con il comando "Serverless"

--- components/chartRow.tsx
+++
@@ -1,5 +0,0 @@
-// import { parseISO, format } from 'date-fns'
-
-// export default function ChartRow({ dateString }: { dateString: string }) {
-//   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-// }
