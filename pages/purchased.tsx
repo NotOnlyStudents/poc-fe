966a8e85fd3aa9aa8e7a3cfa8431631c47e43bd1
Lorenzo Brescanzin <30345822+br3sc4@users.noreply.github.com>
2021-02-25 18:51:56 +0100

    Bug fix

--- pages/purchased.tsx
+++ pages/purchased.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from 'components/layout'
 import utilStyles from 'styles/utils.module.css'
-import { withAuthenticator } from '@aws-amplify/ui-react'
 
-function Purchase() {
+export default function Purchased() {
   return (
     <Layout>
       <Head>
@@ -11,7 +10,5 @@ function Purchase() {
       </Head>
       <h1 className={utilStyles.heading2Xl}>Purchased!</h1>
     </Layout>
-  )
-};
-
-export default Purchase;
+  );
+}
